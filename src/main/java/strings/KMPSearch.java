586227407e838a6154107e18538c18e73eac398e
Michael Levy <michaellevywork@gmail.com>
2020-07-15 00:51:01 +0300

    rename to KMPSearch

--- src/main/java/strings/KMPSearch.java
+++ src/main/java/strings/KMPSearch.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-public class KMP2 {
+public class KMPSearch {
 
     public static List<Integer> search(String str, String word) {
         if (str.length() == 0 || word.length() == 0)
--- src/test/java/strings/KMP2Test.java
+++ src/test/java/strings/KMP2Test.java
@@ -10,43 +10,43 @@ class KMP2Test {
 
     @Test
     public void compare(){
-        assertEquals(1, KMP2.search("abcabcabcabcd", "abcabcd").size());
+        assertEquals(1, KMPSearch.search("abcabcabcabcd", "abcabcd").size());
     }
 
 
     @Test
     public void tableTest1(){
-        assertArrayEquals(new int[]{-1, 0, -1, 1, -1, 0, -1, 1, 4, -1, 0, 2}, KMP2.buildTable("bcbabcbaebc"));
+        assertArrayEquals(new int[]{-1, 0, -1, 1, -1, 0, -1, 1, 4, -1, 0, 2}, KMPSearch.buildTable("bcbabcbaebc"));
     }
 
     @Test
     public void testEmpty(){
-        assertTrue(KMP2.search("",  "").isEmpty());
+        assertTrue(KMPSearch.search("",  "").isEmpty());
     }
 
     @Test
     public void testEmptyText(){
-        assertTrue(KMP2.search("abc",  "").isEmpty());
+        assertTrue(KMPSearch.search("abc",  "").isEmpty());
     }
 
     @Test
     public void testEmptyPattern(){
-        assertTrue(KMP2.search("",  "abc").isEmpty());
+        assertTrue(KMPSearch.search("",  "abc").isEmpty());
     }
 
     @Test
     public void singleEqualsCharacter(){
-        assertTrue(!KMP2.search("a",  "a").isEmpty());
+        assertTrue(!KMPSearch.search("a",  "a").isEmpty());
     }
 
     @Test
     public void singleNotEqualCharacter(){
-        assertTrue(KMP2.search("a",  "b").isEmpty());
+        assertTrue(KMPSearch.search("a",  "b").isEmpty());
     }
 
     @Test
     public void twoMatches(){
-        List<Integer> result = KMP2.search("aa", "a");
+        List<Integer> result = KMPSearch.search("aa", "a");
         assertTrue(result.size() == 2);
         assertEquals(0, result.get(0));
         assertEquals(1, result.get(1));
@@ -54,14 +54,14 @@ class KMP2Test {
 
     @Test
     public void cocacola(){
-        List<Integer> result = KMP2.search("COCOCACOLA", "COCACOLA");
+        List<Integer> result = KMPSearch.search("COCOCACOLA", "COCACOLA");
         assertEquals(1, result.size());
         assertEquals(2, result.get(0));
     }
 
     @Test
     public void ananasCheck(){
-        List<Integer> result = KMP2.search("ANANAANANAS", "ANANAS");
+        List<Integer> result = KMPSearch.search("ANANAANANAS", "ANANAS");
         assertEquals(1, result.size());
         assertEquals(5, result.get(0));
     }
